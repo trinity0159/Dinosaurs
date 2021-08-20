5fc0d8a51e6d92c73db4eb93c1e86529bab1d6f8
Katrina <katrinam0701@gmail.com>
2021-08-20 14:58:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,94 @@
+//Dino Data
+function DinoData() {
+    const dinos = [
+        {
+            "species": "Triceratops",
+            "weight": 13000,
+            "height": 114,
+            "diet": "herbivore",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "First discovered in 1889 by Othniel Charles Marsh"
+        },
+        {
+            "species": "Tyrannosaurus Rex",
+            "weight": 11905,
+            "height": 144,
+            "diet": "carnivore",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "The largest known skull measures in at 5 feet long."
+        },
+        {
+            "species": "Anklyosaurus",
+            "weight": 10500,
+            "height": 55,
+            "diet": "herbivore",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "Anklyosaurus survived for approximately 135 million years."
+        },
+        {
+            "species": "Brachiosaurus",
+            "weight": 70000,
+            "height": "372",
+            "diet": "herbivore",
+            "where": "North America",
+            "when": "Late Jurassic",
+            "fact": "An asteroid was named 9954 Brachiosaurus in 1991."
+        },
+        {
+            "species": "Stegosaurus",
+            "weight": 11600,
+            "height": 79,
+            "diet": "herbivore",
+            "where": "North America, Europe, Asia",
+            "when": "Late Jurassic to Early Cretaceous",
+            "fact": "The Stegosaurus had between 17 and 22 seperate plates and flat spines."
+        },
+        {
+            "species": "Elasmosaurus",
+            "weight": 16000,
+            "height": 59,
+            "diet": "carnivore",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "Elasmosaurus was a marine reptile first discovered in Kansas."
+        },
+        {
+            "species": "Pteranodon",
+            "weight": 44,
+            "height": 20,
+            "diet": "carnivore",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "Actually a flying reptile, the Pteranodon is not a dinosaur."
+        },
+        {
+            "species": "Pigeon",
+            "weight": 0.5,
+            "height": 9,
+            "diet": "herbivore",
+            "where": "Worldwide",
+            "when": "Holocene",
+            "fact": "All birds are living dinosaurs."
+        }
+    ];
+
+    return dinos;
+}
 
     // Create Dino Constructor
+    function Dinosaurs (species, weight, height, diet, where, when, fact){
+        this.species = species;
+        this.weight = weight;
+        this.height = height;
+        this.deit = diet;
+        this.where = where;
+        this.when = when;
+        this.fact = fact;
+    }
+
 
 
     // Create Dino Objects
@@ -26,7 +115,11 @@
   
         // Add tiles to DOM
 
-    // Remove form from screen
+
 
 
 // On button click, prepare and display infographic
+    function DinoGrid(){
+        // Remove form from screen
+        document.getElementById("dino-compare").style.display = "none"
+}
